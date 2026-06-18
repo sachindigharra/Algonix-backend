2117cf9cb7651ec10c5cfdfd9c0ce690bcce516a
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2026-06-18 23:23:27 +0530

    feat(problem): enhance the api endpoints

---
+++ src/main/java/com/algonix/server/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.algonix.server.mapper;
+
+import com.algonix.server.dto.UserProfileResponse;
+import com.algonix.server.entity.User;
+import com.algonix.server.entity.UserProfile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserProfileResponse mapToUserProfileResponse(User user) {
+
+        UserProfile profile = user.getProfile();
+
+        return UserProfileResponse.builder()
+                .userId(user.getId())
+                .email(user.getEmail())
+                .fullName(user.getFullName() != null ? user.getFullName() : null)
+                .avatarUrl(profile != null ? profile.getAvatarUrl() : null)
+                .bio(profile != null ? profile.getBio() : null)
+                .build();
+    }
+}
--- src/main/java/com/algonix/server/repository/ProblemRepository.java
+++ src/main/java/com/algonix/server/repository/ProblemRepository.java
@@ -4,10 +4,14 @@ import com.algonix.server.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, UUID> {
 
 
+    List<Problem> findByUserId(UUID userId);
+
+    List<Problem> findAll();
 }
